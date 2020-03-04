1324003c47e4ddf1ce03241c0ff08ef5b755a112
omnius008 <42618636+omnius008@users.noreply.github.com>
2020-03-04 08:32:19 -0500

    d

---
+++ Chapter6Practice/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Planner
+{
+    public class Building
+    {
+
+        private string Designer { get; set; }
+        
+        private DateTime DateConstructed { get; set; }
+
+        private string Address { get; set; }
+
+        private string Owner { get; set; }
+
+        public int Stories { get; set; }
+
+        public double Width { get; set; }
+
+        public double Depth { get; set; }
+
+        public double Volume
+        {
+            get
+            {
+                return Width * Depth * (3 * Stories);
+            }
+        }
+        public Building(string addressParam)
+        {
+            Address = addressParam;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Building giveAddress = new Building("give it an address")
+            {
+                Stories = 1,
+                Width = 1,
+                Depth = 1
+            };
+        }
+    }
+}
